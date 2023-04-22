45bedf98922d324a4ce8daa6e8c642b4542320a2
samuel.maicas.development <samuel.maicas.development@gmail.com>
2023-04-22 17:59:37 +0200

    Add codewars RGBToHex

---
+++ K5/RGBToHexConversion/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write(Kata.Rgb(0, 0, 0));
+
+public class Kata
+{
+    public static string Rgb(int r, int g, int b)
+    {
+        (int rr, int gg, int bb) = (r > 0 ? Math.Min(255, r) : 0, g > 0 ? Math.Min(255, g) : 0, b > 0 ? Math.Min(255, b) : 0);
+
+        return $"{rr:X2}{gg:X2}{bb:X2}";
+    }
+}
